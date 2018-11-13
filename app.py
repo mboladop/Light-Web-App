b6eec07655a85054dc6ba33d339bf558532b2398
mboladop <mboladop@gmail.com>
2018-11-13 12:03:48 +0000

    flask gives back json

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+readings = [
+    {
+        'date': 12-10-2018,
+        'counter': 197345367
+    },
+    {
+        'date': 13-10-2018,
+        'counter': 197345369
+    }
+]
 
+@app.route('/readings', methods=['GET'])
+def get_readings():
+    return jsonify({'readings': readings})
 
 if __name__ == '__main__':
     app.run(debug=True)
