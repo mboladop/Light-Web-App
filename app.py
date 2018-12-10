b9b04d20cc27b8f9acd4b346a03cde09c812d181
mboladop <mboladop@gmail.com>
2018-12-10 12:25:42 +0000

    clean up of app.py

--- app.py
+++ app.py
@@ -40,15 +40,6 @@ def get_readings(year, month)
      ))
     return resultados
 
-# def get_readings_month():
-#     now = datetime.datetime.now()
-#     year = now.year
-#     month = now.month
-#     yesterday = now.day - 1
-#     dateStr = str(year) + "-" + str(month) + "-" + str(yesterday)
-#     date = dateutil.parser.parse(dateStr)
-#     resultados = dumps(mongo.db.readings.find({'date' : { '$gt' : date}},{'date':'1'}))
-#     return resultados
 
 @app.route('/addreadings', methods=['POST'])
 def add_readings():
@@ -98,61 +89,3 @@ if __name__ == '__main__':
 
 
 
-# parser = reqparse.RequestParser()
-# def post(self):
-#         parser.add_argument('quote', type=str)
-#         args = parser.parse_args()
-
-#         return {
-#             'status': True,
-#             'quote': '{} added. Good'.format(args['quote'])
-#         }
-
-# class Quotes(Resource):
-#     def get(self):
-#         return {
-#             'ataturk': {
-#                 'quote': ['Yurtta sulh, cihanda sulh.', 
-#                     'Egemenlik verilmez, alınır.', 
-#                     'Hayatta en hakiki mürşit ilimdir.']
-#             },
-#             'linus': {
-#                 'quote': ['Talk is cheap. Show me the code.']
-#             }
-
-#         }
-
-
-# api.add_resource(Quotes, '/')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# readings = [
-#     {
-#         'date': 10-11-2018,
-#         'counter': 197345367
-#     },
-#     {
-#         'date': 10-11-2018,
-#         'counter': 197345369
-#     }
-# ]
-
-# @app.route('/readings', methods=['GET'])
-# def get_readings():
-#     return jsonify({'readings': readings})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# @app.route('/readings', methods=['POST'])
-# def create_reading():
-#     if not request.json or not 'title' in request.json:
-#         abort(400)
-#     reading = {
-#         'date': request.json['date'],
-#         'counter': request.json.get('counter', ""),
-#     }
-#     readings.append(reading)
-#     return jsonify({'reading': reading}), 201
